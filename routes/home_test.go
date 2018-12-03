e041be4783821e8392b4e7b8689c07328cd0b7ec
Zeuxis Lo <seekstudio@gmail.com>
2018-12-03 19:18:29 +0800

    improve the home routes test case and support test the robots.txt is or not exists

--- routes/home_test.go
+++ routes/home_test.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"os"
+	"io/ioutil"
 	"net/http"
 	"testing"
 
@@ -12,20 +14,34 @@ var (
 )
 
 func TestHomeIndexGetOK(t *testing.T) {
-	responseRecorder := performRequest(engine, "GET", "/")
-
 	Convey("HomeIndexGet / should be OK", t, func() {
+		responseRecorder := performRequest(engine, "GET", "/")
+
 		So(responseRecorder.Code, ShouldEqual, http.StatusOK)
 		So(responseRecorder.Body.String(), ShouldEqual, "ZenWords")
 	})
 }
 
 func TestHomeRobotsTxtGetOK(t *testing.T) {
-	responseRecorder := performRequest(engine, "GET", "/robots.txt")
-
 	Convey("HomeRobotsTxtGet /robots.txt should be OK", t, func() {
-		So(responseRecorder.Code, ShouldEqual, http.StatusOK)
-		So(responseRecorder.Body.String(), ShouldContainSubstring, "User-agent")
-		So(responseRecorder.Body.String(), ShouldContainSubstring, "Disallow")
+		Convey("When robots.txt is not exists", func() {
+			os.Remove("robots.txt")
+
+			responseRecorder := performRequest(engine, "GET", "/robots.txt")
+
+			So(responseRecorder.Code, ShouldEqual, http.StatusOK)
+			So(responseRecorder.Body.String(), ShouldContainSubstring, "User-agent")
+			So(responseRecorder.Body.String(), ShouldContainSubstring, "Disallow")
+		})
+
+		Convey("When robots.txt is exists", func() {
+			ioutil.WriteFile("robots.txt", []byte("Hello World\nrobots.txt"), 0644)
+
+			responseRecorder := performRequest(engine, "GET", "/robots.txt")
+
+			So(responseRecorder.Code, ShouldEqual, http.StatusOK)
+			So(responseRecorder.Body.String(), ShouldContainSubstring, "Hello World")
+			So(responseRecorder.Body.String(), ShouldContainSubstring, "robots.txt")
+		})
 	})
 }
