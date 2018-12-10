35ceeba7937df9deef7047cf4e6dd6fdf4007803
Zeuxis Lo <seekstudio@gmail.com>
2018-12-10 17:30:04 +0800

    add exact match search api to check the provided content

--- pkg/keywords/keywords.go
+++ pkg/keywords/keywords.go
@@ -6,24 +6,44 @@ import (
 	"io"
 	"bytes"
 	"log"
+	"sort"
+
+	"github.com/euclidr/darts"
 )
 
 var (
-	Words	[][]rune
+	Words		[]string
+	Builder		darts.DoubleArrayBuilder
 )
 
 // NewKeywords to setup the base keywords environment
 func NewKeywords(keywordFile string) {
-	var err error
-
-	Words, err = loadFile(keywordFile)
+	words, err := loadFile(keywordFile)
 	if err != nil {
 		log.Fatalf("Cannot read the words.txt file: %v\n", err)
 	}
+	sort.Strings(words)
+
+	builder := darts.DoubleArrayBuilder{}
+	builder.Build(words)
+
+	Words    = words
+	Builder  = builder
 }
 
-func loadFile(filePath string) ([][]rune, error) {
-	words := [][]rune{}
+// Search return the result string and match state base on content
+func Search(content string) (result string, matched bool) {
+	index, matched := Builder.ExactMatchSearch(content)
+
+	if matched == false {
+		return "", matched
+	}else{
+		return Words[index], matched
+	}
+}
+
+func loadFile(filePath string) ([]string, error) {
+	words := []string{}
 
 	file, err := os.Open(filePath)
 	if err != nil {
@@ -38,7 +58,7 @@ func loadFile(filePath string) ([][]rune, error) {
 			break
 		}else{
 			line  = bytes.TrimSpace(line)
-			words = append(words, bytes.Runes(line))
+			words = append(words, string(line))
 		}
 	}
 
--- routes/api/search.go
+++ routes/api/search.go
@@ -1,12 +1,23 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+
+	"github.com/zeuxisoo/go-zenwords/pkg/keywords"
 )
 
 // SearchPost to handle the search action
 func SearchPost(c *gin.Context) {
-	c.String(http.StatusOK, "ApiSearchPost")
+	content := c.PostForm("content")
+
+	fmt.Println(content)
+
+	result, _ := keywords.Search(content)
+
+	c.JSON(http.StatusOK, gin.H{
+		"search": result,
+	})
 }
